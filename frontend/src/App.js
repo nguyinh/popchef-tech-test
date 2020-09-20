38e561412bf2c38fb2f8f175272314a52799d340
nguyinh <florent.dupontnguyen@gmail.com>
2020-09-20 16:51:44 +0200

    Added spinner for loadings (fetch, create, update, delete products)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,31 @@
 import React, { useEffect, useState } from "react";
 import "./App.css";
 import "semantic-ui-css/semantic.min.css";
-import { Button } from "semantic-ui-react";
+import { Button, Dimmer, Loader, Segment } from "semantic-ui-react";
 import { fetchProducts, addProduct } from "./services";
 import { ProductsTable, ProductEdit } from "./components";
 
 const App = () => {
   const [products, setProducts] = useState([]);
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
 
   const productCreation = () => {
     setIsModalOpen(true);
   };
 
   const submitProduct = async (product) => {
+    setIsLoading(true);
     setIsModalOpen(false);
-    await addProduct(product.label, product.price, product.rating, product.category);
+    await addProduct(
+      product.label,
+      product.price,
+      product.rating,
+      product.category
+    );
 
     setProducts(await fetchProducts());
+    setIsLoading(false);
   };
 
   const resetModal = () => {
@@ -25,13 +33,21 @@ const App = () => {
   };
 
   useEffect(async () => {
+    setIsLoading(true);
     setProducts(await fetchProducts());
+    setIsLoading(false);
   }, []);
 
   return (
     <div className="App">
-      <Button onClick={productCreation}>Ajouter un plat</Button>
-      <ProductsTable products={products} />
+      <Segment>
+        <Dimmer active={isLoading}>
+          <Loader />
+        </Dimmer>
+
+        <Button onClick={productCreation}>Ajouter un plat</Button>
+        <ProductsTable products={products} />
+      </Segment>
 
       {isModalOpen && (
         <ProductEdit submitProduct={submitProduct} resetModal={resetModal} />
